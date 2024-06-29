dcf0ce0111522ca4b40a998d6c1183cdfd91d709
Andres-QG <andres.quesadagonzalez@ucr.ac.cr>
2024-06-28 20:11:03 -0600

    parallel auth server

--- src/authenticatorServer/server/src/Server.cpp
+++ src/authenticatorServer/server/src/Server.cpp
@@ -5,10 +5,23 @@
 #include "FileSystem.hpp"
 #include "Socket.hpp"
 #include "authUtils.hpp"
+#include <condition_variable>
+#include <csignal>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
 
+// Singleton instances
 Socket &server = Socket::getInstance();
 FileSystem &fs = FileSystem::getInstance();
 static const std::string serverName = "Authenticator";
+// TODO(any): Better name for this mutex
+std::mutex canAccessThreadStuff;
+std::mutex canAccessFS;
+std::condition_variable cv;
+bool shutdownRequested = false;
+std::vector<std::thread> threads;
 
 /**
  * @brief Obtains the correct users hash and compares it with the processed
@@ -21,23 +34,28 @@ static const std::string serverName = "Authenticator";
  */
 bool tryAuth(const std::string &user, const std::string &hash) {
   bool authSuccessful = false;
+  std::string userData;
   // Get Auth data from File System
-  if (!fs.open(user)) {
-    LOG("\tFileSystem failed at open");
-    FILELOG("\tFileSystem failed at open");
-    return authSuccessful;
-  }
-  fs.change2ReadMode(user);
-  const std::string userData = fs.read(user, 200);
-  if (userData == "") {
-    LOG("\tFileSystem failed at read");
-    FILELOG("\tFileSystem failed at read");
-    return authSuccessful;
-  }
-  if (!fs.close(user)) {
-    LOG("\tFileSystem failed at close");
-    FILELOG("\tFileSystem failed at close");
-    return authSuccessful;
+  { // Protect FS from race condition (open/close/change2ReadMode)
+    std::lock_guard<std::mutex> lock(canAccessFS);
+
+    if (!fs.open(user)) {
+      LOG("\tFileSystem failed at open");
+      FILELOG("\tFileSystem failed at open");
+      return authSuccessful;
+    }
+    fs.change2ReadMode(user);
+    userData = fs.read(user, 200);
+    if (userData == "") {
+      LOG("\tFileSystem failed at read");
+      FILELOG("\tFileSystem failed at read");
+      return authSuccessful;
+    }
+    if (!fs.close(user)) {
+      LOG("\tFileSystem failed at close");
+      FILELOG("\tFileSystem failed at close");
+      return authSuccessful;
+    }
   }
 
   const std::vector<std::string> fields = splitString(userData, ",");
@@ -92,48 +110,77 @@ std::string protocolGarrobo(const std::string &input) {
   return response;
 }
 
-/**
- * @brief Handles Ctrl + C signal.
- */
-static void signalr(int signal) {
+void handleClient(int clientSocket) {
+  std::string clientRequest;
+  server.receive(clientSocket, clientRequest);
+  LOG("Auth received: " + clientRequest);
+  if (!clientRequest.empty()) {
+    std::string response = protocolGarrobo(clientRequest);
+    LOG("Auth response: " + response)
+    server.send(clientSocket, response);
+  }
+  ::close(clientSocket);
+}
+
+void signalr(int signal) {
   std::cout << "Received signal " << signal << ". Releasing resources..."
             << std::endl;
-  Socket &server = Socket::getInstance();
+  {
+    std::lock_guard<std::mutex> lock(canAccessThreadStuff);
+    shutdownRequested = true;
+  }
+  // All threads close by signal
+  cv.notify_all();
   server.signalHandler(signal);
-  FileSystem &fs = FileSystem::getInstance();
   fs.signalHandler(signal);
+  for (std::thread &t : threads) {
+    if (t.joinable()) {
+      t.join();
+    }
+  }
   exit(0);
 }
 
 int main() {
   std::signal(SIGINT, signalr);
-  ConfigData data = getServerData("../../serverCommon/IP-addresses.txt", serverName);
+  ConfigData data =
+      getServerData("../../serverCommon/IP-addresses.txt", serverName);
 
-
-  if(!server.create()) {
+  if (!server.create()) {
     ERROR("Unable to create server socket");
-    return -1;
-  } else if(!server.bind(data.port, data.ip)) {
-    ERROR("unable to setup server")
-    return -1;
+    return 1;
+  } else if (!server.bind(data.port, data.ip)) {
+    ERROR("Unable to Bind")
+    return 1;
   } else if (!server.listen()) {
     ERROR("Couldn't listen")
-    return -1;;
-  } 
+    return 1;
+  }
 
   while (true) {
+    {
+      std::unique_lock<std::mutex> lock(canAccessThreadStuff);
+      if (shutdownRequested) {
+        break;
+      }
+    }
+
     int clientSocket;
     if (server.accept(clientSocket)) {
-      std::string clientRequest;
-      server.receive(clientSocket, clientRequest);
-      LOG("Auth received: " + clientRequest);
-      if (!clientRequest.empty()) {
-        std::string response = protocolGarrobo(clientRequest);
-        LOG("Auth response: " + response)
-        server.send(clientSocket, response);
-        ::close(clientSocket);
+      // Thread per connection accepted
+      std::thread clientThread(handleClient, clientSocket);
+      {
+        std::lock_guard<std::mutex> lock(canAccessThreadStuff);
+        threads.push_back(std::move(clientThread));
       }
     }
   }
+
+  for (std::thread &t : threads) {
+    if (t.joinable()) {
+      t.join();
+    }
+  }
+
   return 0;
 }
