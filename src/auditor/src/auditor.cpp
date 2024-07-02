046b55319718bcc00ca669bcd0435a841404236a
Andres Serrano <andres.serranorobles@ucr.ac.cr>
2024-07-01 23:38:09 -0600

    Encrypt and decrypt data using Blowfish

--- src/auditor/src/auditor.cpp
+++ src/auditor/src/auditor.cpp
@@ -2,12 +2,42 @@
 // Serrano>
 
 #include "auditor.hpp"
+#include "Blowfish.hpp"
 
 // TODO(any): Encrypt client messages.
+
+inline std::string getKey(const std::string& fileName) {
+  std::ifstream file(fileName);
+
+  if (!file.is_open()) {
+    ERROR("Couldn't open key file")
+    return "";
+  }
+
+  std::string key;
+  file >> key;
+
+  if (key.empty()) {
+    ERROR("Key file is empty")
+    return "";
+  }
+
+  return key;
+}
 
 int main() {
   ConfigData data =
       getServerData("../serverCommon/IP-addresses.txt", "Redirector");
+
+  std::string key = getKey("Key.txt");
+
+  if (key.empty()) {
+    ERROR("Couldn't get key from file")
+    return -1;
+  }
+
+  Blowfish cipher;
+  cipher.setKey(key);
 
   while (true) {
     ClientSocket client(data.ip, data.port);
@@ -15,6 +45,7 @@ int main() {
     std::string message = "AUTH 999999999 "
                           "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f"
                           "6f2ab448a918\nREQUEST USER_DATA 999999999\n";
+    cipher.encrypt(message, message);
     if (client.sendData(message) < 0) {
       ERROR("Unable to send. Redirector possibly failed");
       FILELOG("Error: Unable to send. Redirector possibly failed");
@@ -24,6 +55,7 @@ int main() {
     FILELOG("Sent successful.");
 
     std::string response = client.receiveData();
+    cipher.decrypt(response, response);
     if (response.find("auth server failed") == std::string::npos ||
         response.find("db server failed") == std::string::npos) {
       LOG(response);
